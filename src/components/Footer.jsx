807c79169e429eb272155eb8d84467a16190e46f
Gabriel Cavalcante <gabrielcavalcante.dev@proton.me>
2025-07-31 21:36:41 -0300

    chore: update links github and linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ export default function Footer() {
     <footer className="text-center">
       <TypographyMuted>
         Desenvolvido por{" "}
-        <Link href="https://github.com/gabriellcro">Gabriel Cavalcante</Link>. O
+        <Link href="https://github.com/gbrcavalcante">Gabriel Cavalcante</Link>. O
         código-fonte está disponível no{" "}
-        <Link href="https://github.com/gabriellcro/rescisao-facil">GitHub</Link>
+        <Link href="https://github.com/gabrielcavalcante-dev/rescisao-facil">GitHub</Link>
         .
       </TypographyMuted>
     </footer>
